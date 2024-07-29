6ee5db748cd5422c19335d591202c20b4a9a0854
afaqez <afaqkhan113@gmail.com>
2024-07-29 18:08:37 +0500

    Fix Port

--- main.py
+++ main.py
@@ -132,4 +132,5 @@ def evaluate_quiz(request: EvaluateRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
